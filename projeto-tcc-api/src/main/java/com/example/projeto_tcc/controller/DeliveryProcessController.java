85f287684740bddd33ecab613a05287bdbc147ad
RyanLudieri <ryan.evangelista@sou.unifal-mg.edu.br>
2025-03-31 17:15:16 -0300

    feat: adding the PUT and DELETE methods to the Delivery Process class

--- projeto-tcc-api/src/main/java/com/example/projeto_tcc/controller/DeliveryProcessController.java
+++ projeto-tcc-api/src/main/java/com/example/projeto_tcc/controller/DeliveryProcessController.java
@@ -2,6 +2,7 @@ package com.example.projeto_tcc.controller;
 
 import com.example.projeto_tcc.model.DeliveryProcess;
 import com.example.projeto_tcc.service.DeliveryProcessService;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
@@ -29,4 +30,17 @@ public class DeliveryProcessController {
     public DeliveryProcess getById(@PathVariable Long id) {
         return service.getById(id);
     }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<DeliveryProcess> atualizar(@PathVariable Long id, @RequestBody DeliveryProcess processo) {
+        DeliveryProcess updatedProcess = service.atualizar(id, processo);
+        return ResponseEntity.ok(updatedProcess);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        service.delete(id);
+        return ResponseEntity.noContent().build(); // Retorna HTTP 204 No Content
+    }
+
 }
--- projeto-tcc-api/src/main/java/com/example/projeto_tcc/service/DeliveryProcessService.java
+++ projeto-tcc-api/src/main/java/com/example/projeto_tcc/service/DeliveryProcessService.java
@@ -2,6 +2,7 @@ package com.example.projeto_tcc.service;
 
 import com.example.projeto_tcc.model.DeliveryProcess;
 import com.example.projeto_tcc.repository.DeliveryProcessRepository;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -24,6 +25,25 @@ public class DeliveryProcessService {
 
     public DeliveryProcess getById(Long id) {
         return repository.findById(id).orElseThrow(() -> new RuntimeException("Not found"));
+    }
+
+    public DeliveryProcess atualizar(Long id, DeliveryProcess updatedProcess) {
+        DeliveryProcess existingProcess = repository.findById(id)
+                .orElseThrow(() -> new RuntimeException("DeliveryProcess não encontrado"));
+
+        // Atualiza os campos necessários
+        existingProcess.setIndex(updatedProcess.getIndex());
+        existingProcess.setModelInfo(updatedProcess.getModelInfo());
+        existingProcess.setType(updatedProcess.getType());
+
+        return repository.save(existingProcess); // Salva as alterações
+    }
+
+    public void delete(Long id) {
+        if (!repository.existsById(id)) {
+            throw new EntityNotFoundException("DeliveryProcess não encontrado com ID: " + id);
+        }
+        repository.deleteById(id);
     }
 
 }
