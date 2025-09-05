58a3524541102d81f0da5c3c433fce27072c00a3
RyanLudieri <ryan.evangelista@sou.unifal-mg.edu.br>
2025-09-04 21:11:00 -0300

    feat: adicionando GET para o counteudo do XACDML

--- projeto-tcc-api/src/main/java/com/example/projeto_tcc/controller/XACDMLController.java
+++ projeto-tcc-api/src/main/java/com/example/projeto_tcc/controller/XACDMLController.java
@@ -1,8 +1,10 @@
 package com.example.projeto_tcc.controller;
 
 import com.example.projeto_tcc.entity.XACDMLFile;
+import com.example.projeto_tcc.repository.XACDMLFileRepository;
 import com.example.projeto_tcc.service.XACDMLService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -12,6 +14,7 @@ import org.springframework.web.bind.annotation.*;
 @CrossOrigin(origins = "*") // permite chamadas do frontend
 public class XACDMLController {
 
+    private final XACDMLFileRepository xacdmlRepo;
     private final XACDMLService xacdmlService;
 
     @PostMapping("/generate/{processId}")
@@ -20,5 +23,16 @@ public class XACDMLController {
         XACDMLFile file = xacdmlService.generateXACDML(processId, acdId);
         return ResponseEntity.ok(file.getContent());
     }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<String> getXacdmlContent(@PathVariable Long id) {
+        XACDMLFile file = xacdmlRepo.findById(id)
+                .orElseThrow(() -> new RuntimeException("XACDML não encontrado com id: " + id));
+
+        return ResponseEntity.ok()
+                .contentType(MediaType.APPLICATION_XML) // ou TEXT_XML
+                .body(file.getContent());
+    }
+
 
 }
